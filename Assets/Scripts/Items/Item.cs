335a14337a1bfb09e549bda7d66d89696eafc34c
camL12 <cameronlowe.217@gmail.com>
2022-03-07 22:58:57 +0000

    Set up Item and ItemInstance
    The Item Class is the basic variables needed to create the scriptable object
    
    The ItemInstance class allows the scriptable object to be assigned to any game object, and includes some basic functionality to interact with the scriptable object itself
    
    Also includes 1 test item

---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace Assets.Scripts
+{
+    // adds a new option to the asset menu, for quick creation of new items
+    [CreateAssetMenu(fileName = "New Item", menuName = "Item")]
+    public class Item : ScriptableObject
+    {
+        // all the statistics of the items
+        public int Id;
+        public string Name;
+        public string Description;
+        public int ItemCount;
+        public Sprite Icon;
+    }
+}
+
+   
