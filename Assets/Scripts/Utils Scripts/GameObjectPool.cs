dfd7a225ef585f99a11c31c454880e9465ff2287
patpot <43124869+patpot@users.noreply.github.com>
2022-03-10 00:39:43 +0000

    Create generic GameObjectPool script
    Used to pre-load lots of prefabs on scene start to save frame skips later on

---
+++ Assets/Scripts/Utils Scripts/GameObjectPool.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public class GameObjectPool : MonoBehaviour
+{
+    public GameObject ObjToPool;
+    // This value NEEDS to be higher than the amount of objects you'll ever take from this pool or you'll get some weird behaviours
+    public int PoolCount;
+
+    private List<GameObject> _pool;
+    private int _poolIndex = 0;
+    public void Start()
+    {
+        // Initialise all objects in the pool
+        _pool = new List<GameObject>();
+        for (int i = 0; i < PoolCount; i++)
+            _pool.Add(Instantiate(ObjToPool, transform));
+        // Hide all objects in the pool
+        _pool.ForEach(_ => _.SetActive(false));
+    }
+
+    public GameObject GetObjectFromPool()
+        => _pool[_poolIndex++ % PoolCount];
+
+    public void ReturnToPool(GameObject go)
+    {
+        if (!_pool.Contains(go))
+            Debug.LogError("ERROR! Tried to return an object to a GameObjectPool that wasn't originally part of it!");
+
+        go.transform.SetParent(transform, false);
+        go.SetActive(false);
+    }
+}
